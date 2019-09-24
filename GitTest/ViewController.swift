371ffd77a78a4c86f83b516175e706286d32f932
jiyeonlab <jiyeonlab@gmail.com>
2019-09-24 12:40:34 +0900

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by JiyeonKim on 24/09/2019.
+//  Copyright © 2019 JiYeonKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
